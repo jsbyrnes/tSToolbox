bafd1d9ecd45fcaad55ea24e68747ac200be4f47
jsbyrnes <byrnes.joseph@gmail.com>
2019-11-21 09:53:25 -0600

    Added the ability to collapse the y axis in the linear inversion

--- LinearInversion/smoothTSinversion.m
+++ LinearInversion/smoothTSinversion.m
@@ -7,17 +7,17 @@ clear, close all
 %EDIT THESE LINES TO CONTROL THE INVERSION
 smoothness          = 5;
 station_smallness   = 5;
-model_smallness     = 0.25;
+model_smallness     = 1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %EDIT THIS LINE TO LOAD THE DATA
-fnames = dir('../../TDcode2D/Rio_nofilt/*Measurement.mat');
+fnames = dir('../MM_raw/*Measurement.mat');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%
 %Save results as 
-name = 'LinearInv';
+name = 'LinearInv2';
 %%%%%%
 
 %%%%%%
@@ -25,13 +25,16 @@ name = 'LinearInv';
 v                   = -0.15:0.01:0.15;
 clim                = [-0.15 0.15];
 save_for_gmt        = 0;%not possible in this verison of the code
-rotation            = 0;
+rotation            = 55;
+collapse_y          = 0;
+buffer              = 20;%in km on the outside
+
 label_for_colorbar  = '\Deltat*, s';
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Minor inversion parameters
-nodeSpacing         = 25;
-maxDist             = nodeSpacing*2;
+nodeSpacing         = 10;
+maxDist             = nodeSpacing*5;
 QC                  = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -124,13 +127,17 @@ mstruct.scalefactor = 6371;
 [dataX, dataY] = mfwdtran(mstruct,allLats,allLons);
 %figure; plot(dataX,dataY,'k.');
 
-%make the grid on which to invert
-buffr=1; %buffer size on all sides, on km. (edges will be at edge of footprint+buffer)
+if collapse_y
+   
+    dataY = zeros(size(dataY));
+    
+end
 
-minX=min(dataX)-buffr;
-maxX=max(dataX)+buffr;
-minY=min(dataY)-buffr;
-maxY=max(dataY)+buffr;
+%make the grid on which to invert
+minX=min(dataX)-buffer;
+maxX=max(dataX)+buffer;
+minY=min(dataY)-buffer;
+maxY=max(dataY)+buffer;
 
 xVec=minX:nodeSpacing:maxX;
 yVec=minY:nodeSpacing:maxY;
@@ -159,14 +166,6 @@ for k=1:length(dataI)
 
 end
 
-%get the lat and lon for the stations
-for k = 1:length(uSta)
-   
-    lon_sta(k) = allLons(find(dataI==k, 1));
-    lat_sta(k) = allLats(find(dataI==k, 1));
-    
-end
-
 % First make the G, one row at a time:
 % x and y of each model point as row vectors
 modelX=xMat(:); modelX=modelX';
@@ -179,56 +178,55 @@ modelY=yMat(:); modelY=modelY';
 dPow = 1;
 
 %allocate G
-G=nan(nData,nModel);
+G = nan(nData,nModel);
 
-for k=1:nData
+for k = 1:nData
     
-    stationX=dataX(k);
-    stationY=dataY(k);
+    stationX = dataX(k);
+    stationY = dataY(k);
     
     sta_m_dist=sqrt( (modelX-stationX).^2 + (modelY-stationY).^2 );
 
-    Grow=1./sta_m_dist;
+    Grow = 1./sta_m_dist;
     
-    ix=(sta_m_dist>maxDist);
-    Grow(ix)=0;
+    ix       = (sta_m_dist>maxDist);
+    Grow(ix) = 0;
     
-    Grow=Grow.^dPow;
+    Grow = Grow.^dPow;
     %now normalize the row of G
-    Grow=Grow/sum(Grow);
-    
-    G(k,:)=Grow;
+    Grow   = Grow/sum(Grow);   
+    G(k,:) = Grow;
     
 end
 
 %event term
 %allocate
-Gevt=nan(nData,nEvents);
+Gevt = nan(nData,nEvents);
 %build row-by-row
-for k=1:nData
-   Gevt(k,:)=0;
-   Gevt(k,dataE(k))=1;
+for k = 1:nData
+   Gevt(k,:)        = 0;
+   Gevt(k,dataE(k)) = 1;
 end
 
 %station term
 %allocate
-Gsta=nan(nData,nSta);
+Gsta = nan(nData,nSta);
 %build row-by-row
-for k=1:nData
-   Gsta(k,:)=0;
-   Gsta(k,dataI(k))=1;
+for k = 1:nData
+   Gsta(k,:)        = 0;
+   Gsta(k,dataI(k)) = 1;
 end
 
 %Put sensitivity matrix together:
-G=[G Gevt Gsta];
+G = [G Gevt Gsta];
 
 %station smallness constraint
-GSsml=eye(nSta);
-GSsml=GSsml*station_smallness; % smallness control factor
+GSsml = eye(nSta);
+GSsml = GSsml*station_smallness; % smallness control factor
 
 %model smallness
-GMsml=eye(nModel);
-GMsml=GMsml*model_smallness; % smallness control factor
+GMsml = eye(nModel);
+GMsml = GMsml*model_smallness; % smallness control factor
 
 %add regularization matrices
 Gexpanded = [G; [zeros(nSta,nModel) zeros(nSta,nEvents) GSsml]];
@@ -236,56 +234,67 @@ Gexpanded = [Gexpanded; [GMsml zeros(nModel,nEvents) zeros(nModel,nSta)]];
 
 %smoothness constraint
 makeSmoothness % this script produces Gsmth
-Gsmth=Gsmth*smoothness;
+Gsmth     = Gsmth*smoothness;
 Gexpanded = [Gexpanded; [Gsmth zeros(nModel,nEvents) zeros(nModel,nSta)]];
 
 %% build expanded d
-dExpanded=[dataV; zeros(nSta,1); zeros(nModel,1); zeros(nModel,1)];
+dExpanded = [dataV; zeros(nSta,1); zeros(nModel,1); zeros(nModel,1)];
 %the added zeros are for station term smallness, model smallness and model
 %smoothness.
 
 %% and invert
-Minverted=Gexpanded\dExpanded;% lsqr(Gexpanded, dExpanded, 1e-10, 3000);%
-MI=Minverted;
+Minverted = Gexpanded\dExpanded;% lsqr(Gexpanded, dExpanded, 1e-10, 3000);%
+MI        = Minverted;
 
-MImodel=Minverted(1:nModel);
-Minverted(1:nModel)=[];
+MImodel             = Minverted(1:nModel);
+Minverted(1:nModel) = [];
 
-MIevt=Minverted(1:nEvents);
-Minverted(1:nEvents)=[];
+MIevt                = Minverted(1:nEvents);
+Minverted(1:nEvents) = [];
 
-MIsta=Minverted;
-
-MImodel=reshape(MImodel,size(xMat));
+MIsta   = Minverted;
+MImodel = reshape(MImodel,size(xMat));
 
 %% make some pretty plots
 
-figure(1); contourf(xMat,yMat,MImodel, v); h = colorbar; %caxis([0 2])
-h.Label.String = '\Deltat*';
-load('CMfine');
-colormap(cm)
-caxis(clim)
-hold on
+figure(1)
 
-[dataX, dataY] = mfwdtran(mstruct,lat_sta,lon_sta);
-scatter(dataX,dataY,10, MIsta,'filled', 'MarkerEdgeColor', 'k')
-set(gca,'YDir','normal')
-daspect([1 1 1])
+if collapse_y
 
-C=load('coast');
-[C.x,C.y]=mfwdtran(mstruct,C.lat,C.long);
-C.x(C.x>maxX)=nan;
-C.x(C.x<minX)=nan;
-C.y(C.y>maxY)=nan;
-C.y(C.y<minY)=nan;
-plot(C.x,C.y,'k-','LineWidth',2)
+    plot(xMat(1, :), MImodel(1, :));
+    xlabel('Distance, km');
+    ylabel('\Deltat*')
+    
+else
+    
+    contourf(xMat,yMat,MImodel, v); h = colorbar; %caxis([0 2])
+    h.Label.String = '\Deltat*';
+    load('CMfine');
+    colormap(cm)
+    caxis(clim)
+    hold on
 
-xlim([ minX maxX ]);
-ylim([ minY maxY ]);
+    [dX, indY] = unique(dataX);
 
+    scatter(dX, dataY(indY),10, MIsta,'filled', 'MarkerEdgeColor', 'k')
+    set(gca,'YDir','normal')
+    daspect([1 1 1])
 
+    C=load('coast');
+    [C.x,C.y]=mfwdtran(mstruct,C.lat,C.long);
+    C.x(C.x>maxX)=nan;
+    C.x(C.x<minX)=nan;
+    C.y(C.y>maxY)=nan;
+    C.y(C.y<minY)=nan;
+    plot(C.x,C.y,'k-','LineWidth',2)
+
+    xlim([ minX maxX ]);
+    ylim([ minY maxY ]);
+
+end
+    
 figure(2)
 histogram(MIsta)
 xlabel('Size of the station terms');
 
-save(name, 'MImodel', 'xMat', 'yMat', 'mstruct', 'MIevt', 'MIsta', 'dataE', 'dataI', 'dataV', 'dataX', 'dataY', 'uSta', 'lon_sta', 'lat_sta');
+save(name, 'MImodel', 'xMat', 'yMat', 'mstruct', 'MIevt', 'MIsta', 'dataE', 'dataI', 'dataV', 'dataX', 'dataY', 'uSta');
